c861bebb973d8eb5b5f45b60bcc6a1780ce2f526
manja1990 <manja1990@naver.com>
2022-01-21 10:24:45 +0900

    머지테스트

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,9 +114,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [BASE_DIR/'static',]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
